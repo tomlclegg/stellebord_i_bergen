b7a5ad57cbe1b49a78805e56eb7cb0d478fa7f56
tomlclegg <tomclegg@protonmail.com>
2022-08-14 17:41:54 +0200

    First commit

---
+++ baby_i_bergen/app.R
@@ -0,0 +1,37 @@
+
+library(shiny)
+library(leaflet)
+
+
+
+# Read in data ------------------------------------------------------------
+
+
+loc <- read.csv("data/baby_sted_i_bergen.csv")
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    leafletOutput("mymap")
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+  output$mymap <- renderLeaflet({
+
+    leaflet() |>
+      addTiles() |>
+      addMarkers(data = loc,
+                 lng = ~longitude,
+                 lat = ~latitude,
+                 label = ~title,
+                 popup = ~description)
+
+  })
+
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
